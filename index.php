35b44732a8ff5625ff8f963ec47b05700f8bdc24
ahmed ali <you@example.com>
2021-12-08 19:15:15 +0300

    rebuild with challenge 7

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ include './include/inc.header.php'
 
     <div class="container landing">
         <p class="mb-5"></p>
-        <h1 data-aos="fade-right" data-aos-delay="50"
-            data-aos-duration="1000" class="title mt-5">Party Time</h1>
+        <h1  
+             class="title mt-5">Party Time</h1>
 
-        <div data-aos="fade-right" data-aos-delay="50"
-             data-aos-duration="1000" class="offer">
+        <div  
+              class="offer">
             <div class="shape">
                 <div class="shape-text">
                     Buy any 2 burgers and get 1.5L Pepsi Free
@@ -19,8 +19,7 @@ include './include/inc.header.php'
             </div>
         </div>
 
-        <button data-aos="fade-up-right" data-aos-once="true" data-aos-delay="50"
-                data-aos-duration="1000"  id='order'>Order Now</button>
+        <button    id='order'>Order Now</button>
     </div>
 
 
@@ -41,8 +40,7 @@ include './include/inc.header.php'
             foreach ($recent as $COOKIE_Meal){
                 $meal = $Meal->getMealById($COOKIE_Meal->id);
                 ?>
-                <div data-aos="zoom-in-up" data-aos-once="true" data-aos-delay="50"
-                     data-aos-duration="1000" class="card p-0 my-2 col-lg-3 col-md-4 col-11">
+                <div  class="card p-0 my-2 col-lg-3 col-md-4 col-11">
                     <a href=" detail.php?id=<?php echo  $meal['id']  ?>">
                         <img  src="<?php echo "Images/".$meal['image'] ?>"   class="card-img-top" alt="<?php echo $meal['title']; ?>">
                     </a>
@@ -90,30 +88,26 @@ include './include/inc.header.php'
 
 
     <div class="menu-links row container mx-auto">
-        <a  data-aos="zoom-in" data-aos-once="true" data-aos-delay="50"
-            data-aos-duration="1000" class="col my-0 mx-1 p-1" href="">burger</a>
-        <a data-aos="zoom-in" data-aos-once="true" data-aos-delay="250"
-            data-aos-duration="1000" class="col my-0 mx-1 p-1" href="">pizza</a>
-        <a data-aos="zoom-in" data-aos-once="true" data-aos-delay="450"
-            data-aos-duration="1000" class="col my-0 mx-1 p-1" href="">fast food</a>
-        <a data-aos="zoom-in" data-aos-once="true" data-aos-delay="650"
-            data-aos-duration="1000" class="col my-0 mx-1 p-1" href="">cupcake</a>
-        <a data-aos="zoom-in" data-aos-once="true" data-aos-delay="850"
-            data-aos-duration="1000" class="col my-0 mx-1 p-1" href="">sandwich</a>
-        <a data-aos="zoom-in" data-aos-once="true" data-aos-delay="1050"
-            data-aos-duration="1000" class="col my-0 mx-1 p-1" href="">spaghetti</a>
+        <a   class="col my-0 mx-1 p-1" href="">burger</a>
+        <a  
+             class="col my-0 mx-1 p-1" href="">pizza</a>
+        <a   class="col my-0 mx-1 p-1" href="">fast food</a>
+        <a  
+             class="col my-0 mx-1 p-1" href="">cupcake</a>
+        <a  class="col my-0 mx-1 p-1" href="">sandwich</a>
+        <a  class="col my-0 mx-1 p-1" href="">spaghetti</a>
     </div>
 
 
     <div class="image-background">
 
 
-        <img data-aos="fade-down-right" data-aos-once="true" data-aos-delay="50"
-             data-aos-duration="1500" class="w-100" src="Images/delivery.png" alt="delivery pizza guy" id="pizza-guy">
+        <img  
+              class="w-100" src="Images/delivery.png" alt="delivery pizza guy" id="pizza-guy">
 
 
-        <div data-aos="fade-down-left" data-aos-once="true" data-aos-delay="50"
-             data-aos-duration="1500">
+        <div 
+             >
             <div class="Tshape">
                 <div class="Tshape-text">
                     <h2><strong>guarantee 30 minutes delivery</strong></h2>
@@ -143,9 +137,8 @@ include './include/inc.header.php'
             $meals = $Meal->getAllMeals();
 
             foreach ($meals as $key=>$meal) { ?>
-            <div data-aos="zoom-in-down"
-                 data-aos-anchor-placement="center-bottom"  data-aos-once="true" data-aos-delay="<?php echo 100*$key ?>"
-                 data-aos-duration="1500" class="card p-0   my-2 col-lg-2 col-md-4 col-11">
+            <div <?php echo 100*$key ?>
+                  class="card p-0   my-2 col-lg-2 col-md-4 col-11">
 
                 <a href=" detail.php?id=<?php echo  $meal['id']  ?>">
                     <img  src="<?php echo "Images/".$meal['image'] ?>"   class="card-img-top" alt="<?php echo $meal['title']; ?>">
@@ -195,43 +188,13 @@ include './include/inc.header.php'
 
             <div class="row justify-content-center align-items-center">
 
-                <div id="carouselExampleIndicators" class="carousel slide col-lg-6 col-sm-11 " data-ride="carousel">
+                <div class=" col-lg-6 col-sm-11 ">
 
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="Images/man-eating-burger.png" alt="First slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="Images/man-eating-burger.png" alt="Second slide">
-                        </div>
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="Images/man-eating-burger.png" alt="Second slide">
-                        </div>
-
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-                    </ol>
-
+                   
+                   <img class="d-block w-100" src="Images/man-eating-burger.png" >
 
                 </div>
-                <p class="col-lg-6 col-sm-11">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nam aut
-                    deserunt minus-->
-                    provident aliquid
-                    maxime
-                    et? Nisi incidunt laborum beatae debitis. Voluptatibus, qui recusandae assumenda a
-                    minima vero
-                    quasi.</p>
+                <p class="col-lg-6 col-sm-11">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis nam aut deserunt minus--> provident aliquid maxime et? Nisi incidunt laborum beatae debitis. Voluptatibus, qui recusandae assumenda a minima vero  quasi.</p>
 
             </div>
 
